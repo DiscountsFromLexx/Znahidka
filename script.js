5b8e0753aa3a6a771a460f4b876da073e9e66cca
DISCOUNTS-FROM-AL-LEXX <dyntchev@gmail.com>
2026-02-21 23:51:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -128,10 +128,10 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     
         addLog('Data to Send', data);
-        addLog('Fetch URL', 'https://d74e0fadbba0.ngrok-free.app/submit');
+        addLog('Fetch URL', 'https://2fde-34-134-106-120.ngrok-free.app/submit');
     
         try {
-            const response = await fetch('https://d74e0fadbba0.ngrok-free.app/submit', {
+            const response = await fetch('https://2fde-34-134-106-120.ngrok-free.app/submit', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
